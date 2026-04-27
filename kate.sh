918e604067842f5e6f201820c69a6c7f8232b2ee
Nkemjika <nintechglobal@gmail.com>
2026-04-27 03:45:11 +0100

    initial commit: git script

---
+++ kate.sh
@@ -0,0 +1,8 @@
+cd my_GitLab
+:~/my_GitLab git init #git initialise
+:~/my_GitLab git add filename
+:~/my_GitLab git status
+:~/my_GitLab git commit -m "initial commit: restartconfig_file"
+:~/my_GitLab git remote add origin git@github.com:nintechg/container.git
+:~/my_GitLab git branch -M main
+:~/my_GitLab ..
